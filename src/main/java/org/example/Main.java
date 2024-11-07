cc037dc54d2a123cc68c380f3928680420af7efb
Conor Daly <dalyconor1@gmail.com>
2024-11-07 16:47:43 +0000

    Test No 2 for webhook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Big Test");
+        System.out.println("Big Test No 2");
     }
 }
